a7f18de40cc61c38c5021db53aeebba4946c4acc
KMASAHIRO <k.masa1215@gmail.com>
2023-07-16 02:07:17 +0900

    bug fix

--- tools/auto_train.py
+++ tools/auto_train.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import configparser
 import subprocess
@@ -7,29 +8,27 @@ if __name__ == "__main__":
     config.read("config.ini", encoding='utf-8')
 
     train_path = config.get("DEFAULT", "do_train_path")
-    map_dir = config.get("DEFAULT", "map_dir")
-    
     map_name = config.get("DEFAULT", "map_name")
-    device = config.get("DEFAULT", "device")
 
     sections = config.sections()
     experiments = dict()
     for sec in sections:
         if sec != "DEFAULT":
             experiments[sec] = dict(config.items(sec))
-            if "device" not in experiments[sec].keys():
-                experiments[sec]["device"] = device
+            experiments[sec].pop("do_train_path")
             if "log_dir" not in experiments[sec].keys():
                 experiments[sec]["log_dir"] = "./"
             if "reward_csv" not in experiments[sec].keys():
                 experiments[sec]["reward_csv"] = map_name + "_IPPO_" + sec + "_reward.csv"
             if "run_name" not in experiments[sec].keys():
                 experiments[sec]["run_name"] = "IPPO_" + sec
+            
+            experiments[sec]["map_dir"] = os.path.abspath(experiments[sec]["map_dir"])
 
     for dir_name in experiments.keys():
         subprocess.run(["mkdir", dir_name])
         python_cmd = [
-            "python", train_path, "--map_name", map_name, "--map_dir", map_dir
+            "python", os.path.abspath(train_path)
             ]
         
         for op,val in experiments[dir_name].items():
--- tools/auto_train_gym.py
+++ tools/auto_train_gym.py
@@ -1,3 +1,4 @@
+import os
 import configparser
 import subprocess
 
@@ -6,17 +7,14 @@ if __name__ == "__main__":
     config_gym.read("config_gym.ini", encoding='utf-8')
 
     train_path = config_gym.get("DEFAULT", "do_train_path")
-
     env_name = config_gym.get("DEFAULT", "env_name")
-    device = config_gym.get("DEFAULT", "device")
     
     sections = config_gym.sections()
     experiments = dict()
     for sec in sections:
         if sec != "DEFAULT":
             experiments[sec] = dict(config_gym.items(sec))
-            if "device" not in experiments[sec].keys():
-                experiments[sec]["device"] = device
+            experiments[sec].pop("do_train_path")
             if "learn_curve_csv" not in experiments[sec].keys():
                 experiments[sec]["learn_curve_csv"] = env_name + "_" + sec + "_learncurve.csv"
             if "model_save_path" not in experiments[sec].keys():
@@ -28,7 +26,7 @@ if __name__ == "__main__":
     for dir_name in experiments.keys():
         subprocess.run(["mkdir", dir_name])
         python_cmd = [
-            "python", train_path, "--env_name", env_name
+            "python", os.path.abspath(train_path)
             ]
         
         for op,val in experiments[dir_name].items():

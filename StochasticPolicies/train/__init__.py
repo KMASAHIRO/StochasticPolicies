6d71e7746b31abf5422ff857746141813acbd7e2
KMASAHIRO <k.masa1215@gmail.com>
2023-07-16 01:16:57 +0900

    bug fix

--- StochasticPolicies/train/__init__.py
+++ StochasticPolicies/train/__init__.py
@@ -1,4 +1,3 @@
 from . import analysis
 from . import train_gym
-from . import test
 from . import train
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='StochasticPolicies',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
     url='https://github.com/KMASAHIRO/StochasticPolicies',
